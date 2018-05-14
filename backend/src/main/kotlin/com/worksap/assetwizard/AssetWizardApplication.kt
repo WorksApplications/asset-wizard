6c6cc0e1db0eafe90104588717965fe027d8dcbb
Kengo TODA <skypencil@gmail.com>
2018-05-14 15:39:31 +0800

    replace annotations to enable unit test on spring-boot-starter-test
    according to official document, SpringBootApplication annotation can replace
    Configuration, EnableWebMvc and ComponentScan.
    
    https://spring.io/guides/gs/spring-boot/

--- backend/src/main/kotlin/com/worksap/assetwizard/AssetWizardApplication.kt
+++ backend/src/main/kotlin/com/worksap/assetwizard/AssetWizardApplication.kt
@@ -1,18 +1,13 @@
 package com.worksap.assetwizard
 
 import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.Configuration
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.boot.autoconfigure.SpringBootApplication
 
-
-// Entry point
-@EnableAutoConfiguration
-@Configuration
-@ComponentScan("com.worksap.assetwizard.controller, com.worksap.assetwizard.service, com.worksap.assetwizard.Repository")
-class AssetWizardApplication
-
-fun main(args: Array<String>) {
-    SpringApplication.run(AssetWizardApplication::class.java, *args)
+@SpringBootApplication
+class AssetWizardApplication {
+    companion object {
+        @JvmStatic fun main(args: Array<String>) {
+            SpringApplication.run(AssetWizardApplication::class.java, *args)
+        }
+    }
 }
