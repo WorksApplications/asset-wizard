96fee3917e4a0e8b6a89fe28ef40e02420e00a5b
Ayush Choubey <ayush.choubey@gmail.com>
2018-05-17 21:34:12 +0800

    refs #5 Fixing Auto increment

--- backend/src/main/kotlin/com/worksap/assetwizard/enities/AssetsEntity.kt
+++ backend/src/main/kotlin/com/worksap/assetwizard/enities/AssetsEntity.kt
@@ -10,7 +10,7 @@ import javax.persistence.*
 data class AssetsEntity (
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id:Int?,
 
     @Column(name="name")
